97387a778af08816fc77ed37ff08a38f0a7a8e3b
Arsenii Vladimirov <78355358+grokhi@users.noreply.github.com>
2023-09-08 10:42:50 +0600

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -38,7 +38,7 @@ if RELEASE_DATE is None:
 HOMEPAGE_URL: str = "https://www.kaggle.com/competitions/severstal-steel-defect-detection/overview"
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = 3315995
+PREVIEW_IMAGE_ID: int = 3315835
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
 GITHUB_URL: str = "https://github.com/dataset-ninja/severstal"
